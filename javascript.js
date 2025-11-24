d4afe796550a40dc3efaf74fb92501e882c922e2
contactJoel <contato.yerijhonrian@gmail.com>
2025-11-24 05:05:33 -0300

    Animation/Header/JS

--- javascript.js
+++ javascript.js
@@ -2,6 +2,9 @@
 const form = document.querySelector (".formulario-fale-conosco")
 const mascara = document.querySelector (".mascara-formulario")
 
+const img = document.querySelector (".mario-desentupidor")
+const imgmascara = document.querySelector (".mascara-desentupidor")
+
 function cliqueinobotao(){
     form.style.left = "50%"
     form.style.transform = "translateX(-50%)"
@@ -11,4 +14,15 @@ function esconderform(){
     form.style.left = "-300%"
     form.style.transform = "translateX(0%)"
     mascara.style.visibility = "hidden"
+}
+function nossosservicos(){
+    img.style.botton = "0"
+    img.style.left = "-20px"
+    img.style.transfrom = "translatex(-50%)"
+    imgmascara.style.visibility = "visible"
+}
+function esconderservicos(){
+    img.style.left = "-300%"
+    img.style.transform = "translateX(0%)"
+    imgmascara.style.visibility = "hidden"
 }
